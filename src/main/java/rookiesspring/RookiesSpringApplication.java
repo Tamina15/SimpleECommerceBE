c2ecefbc439a71d71aeb6e7508d28aacb045ed85
Tamina15 <minhnhat66442658@gmail.com>
2024-05-14 09:16:23 +0700

    Init

---
+++ src/main/java/rookiesspring/RookiesSpringApplication.java
@@ -0,0 +1,13 @@
+package rookiesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookiesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookiesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rookiesspring/RookiesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package rookiesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookiesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
