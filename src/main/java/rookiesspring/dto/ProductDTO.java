29a32e9468cf63326d98d889e6482f865cc12d74
Tamina15 <minhnhat66442658@gmail.com>
2024-05-16 14:42:57 +0700

    Create Input DTO Objects

---
+++ src/main/java/rookiesspring/dto/ProductDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rookiesspring.dto;
+
+/**
+ *
+ * @author HP
+ */
+public record ProductDTO (String name, String description, double price, long[] category_id) {
+    
+}
--- src/main/java/rookiesspring/dto/SupplierDTO.java
+++ src/main/java/rookiesspring/dto/SupplierDTO.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package rookiesspring.dto;
 
 /**
@@ -9,6 +10,7 @@ package rookiesspring.dto;
  * @author HP
  * @author Tamina
  */
-public record UserDTO(String username, String email, String password, String firstname, String lastname, boolean gender, int age, String phone, String address_number, String street, String ward, String district, String city, String province, String country) {
+
+public record SupplierDTO(String name) {
 
 }
