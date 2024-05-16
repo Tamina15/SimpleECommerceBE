52b09c2e1f25fe4194728cb820c67b939a9d761d
Tamina15 <minhnhat66442658@gmail.com>
2024-05-16 14:45:32 +0700

    Create Service Interfaces

--- src/main/java/rookiesspring/service/interfaces/ImportBillServiceInterface.java
+++ src/main/java/rookiesspring/service/interfaces/ImportBillServiceInterface.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package rookiesspring.service.interfaces;
 
 /**
@@ -10,7 +9,7 @@ package rookiesspring.service.interfaces;
  * @author HP
  * @author Tamina
  */
+public interface ImportBillServiceInterface extends BaseServiceInterface{
 
-public interface UserServiceInterface extends BaseServiceInterface{
 
 }
