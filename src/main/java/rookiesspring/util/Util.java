95544c802d38886ac13b3e11f29fca79c846e137
Tamina15 <minhnhat66442658@gmail.com>
2024-05-17 16:15:33 +0700

    Create Util.java

---
+++ src/main/java/rookiesspring/util/Util.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rookiesspring.util;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author HP
+ * @author Tamina
+ */
+public class Util {
+
+    public static LocalDateTime minDateTime = LocalDateTime.of(0, 1, 1, 0, 0, 0, 0);
+
+}
