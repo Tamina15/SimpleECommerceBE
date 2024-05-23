28c8d3efdea592bf8cb73b2982cd1a0e5401108f
Tamina15 <minhnhat66442658@gmail.com>
2024-05-23 09:48:36 +0700

    Update Util.java

--- src/main/java/rookiesspring/util/Util.java
+++ src/main/java/rookiesspring/util/Util.java
@@ -5,6 +5,7 @@
 package rookiesspring.util;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -14,10 +15,17 @@ import java.time.LocalDateTime;
 public class Util {
 
     public static LocalDateTime minDateTime = LocalDateTime.of(0, 1, 1, 0, 0, 0, 0);
-    public static ResponseObject message(String message){
+
+    public static ResponseObject message(String message) {
         return new ResponseObject(message);
     }
+    static String regex = "\\b(.)(.*?)\\b";
+
+    public static String UpperCaseFirstLetter(String s) {
+        return Pattern.compile(regex).matcher(s).replaceAll(matche -> matche.group(1).toUpperCase() + matche.group(2));
+    }
 }
-record ResponseObject(String message){
-    
+
+record ResponseObject(String message) {
+
 }
